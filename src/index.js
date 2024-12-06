f592fae5f97097b4c98cfa5dfd9f40833b860519
Adwoa-Safowaah <adwoaadysne@gmail.com>
2024-12-06 00:04:05 +0000

    I manipulated DOM in my index.html file, including styles, and added more styles in my JavaScript.

---
+++ src/index.js
@@ -0,0 +1,33 @@
+//Manipulate the title
+let title = document.getElementById("title");
+title.style.color = "white";
+title.style.backgroundColor = "blue";
+title.style.padding = "10px";
+title.style.borderRadius = "8px";
+title.innerHTML = "Learning how to manipulate the DOM using JS 💪"
+
+//Manipulate content blocks
+let Q1 = document.getElementById("Q1");
+Q1.style.backgroundColor = "lightgray";
+Q1.style.color = "black";
+Q1.innerHTML = "I know now what the DOM is🙌";
+
+let Q2 = document.getElementById("Q2");
+Q2.style.backgroundColor = "yellow";
+Q2.style.color = "black";
+Q2.innerHTML = "I know how to access / select elements in the DOM. 👊";
+
+let Q3 = document.getElementById("Q3");
+Q3.style.backgroundColor = "green";
+Q3.style.color = "white";
+Q3.innerHTML = "I know how to use the style methods to change the style of an element in the DOM ✅";
+
+let Q4 = document.getElementById("Q4");
+Q4.style.backgroundColor = "purple";
+Q4.style.color = "white";
+Q4.innerHTML = "I know how the styling properties differ in JS from CSS👍";
+
+let Q5 = document.getElementById("Q5");
+Q5.style.backgroundColor = "black";
+Q5.style.color = "white";
+Q5.innerHTML = "Yes I did it!! I manipulated the DOM and styled my elements🥰🥰🥰🥰";
